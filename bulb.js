f11e78e95532b642bff7a5624082c9bb6c411d01
Srilatha Immadi <srilathaimmadi399@gmail.com>
2026-07-03 12:34:56 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,9 @@
+function turnOn() {
+    document.getElementById("bulb").src =
+        "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+
+function turnOff() {
+    document.getElementById("bulb").src =
+        "https://www.w3schools.com/js/pic_bulboff.gif";
+}
